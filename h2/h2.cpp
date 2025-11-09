8ff3df92f5b757bc09ad797732d74a706aab9f3e
Tommi Lipponen <tlippone24@students.oamk.fi>
2025-11-09 11:30:12 +0200

    Tehtävä 1, huono tapa, kommentoituna

--- h2/h2.cpp
+++ h2/h2.cpp
@@ -1,12 +1,66 @@
-﻿// h2.cpp : Defines the entry point for the application.
-//
-
-#include "h2.h"
+﻿#include "h2.h"
 
 using namespace std;
 
+// Tehtävä 1: Käyttöesimerkki julkisilla jäsenmuuttujilla (HUONO TAPA - kommentoitu pois)
+/*
+int main() {
+    Henkilo henkilo1;
+    henkilo1.nimi = "Matti Meikäläinen";  // Suora muuttujaan pääsy - huono tapa!
+    henkilo1.ika = 25;
+    
+    cout << "Nimi: " << henkilo1.nimi << endl;
+    cout << "Ikä: " << henkilo1.ika << endl;
+    
+    return 0;
+}
+*/
+
+// Tehtävä 3: Konstruktorin toteutus
+Henkilo::Henkilo(string nimi, int ika) {
+    this->nimi = nimi;
+    this->ika = ika;
+}
+
+// Tehtävä 2: Getterien toteutus
+string Henkilo::getNimi() const {
+    return nimi;
+}
+
+int Henkilo::getIka() const {
+    return ika;
+}
+
+// Tehtävä 2: Setterien toteutus
+void Henkilo::setNimi(string uusiNimi) {
+    nimi = uusiNimi;
+}
+
+void Henkilo::setIka(int uusiIka) {
+    ika = uusiIka;
+}
+
+// Tehtävä 2: Tulostusmetodin toteutus
+void Henkilo::tulostaTiedot() const {
+    cout << "Nimi: " << nimi << endl;
+    cout << "Ikä: " << ika << endl;
+}
+
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+    cout << "=== Tehtävä 2: Käytetään settereita ===" << endl;
+    Henkilo henkilo1("", 0);  // Luodaan tyhjällä konstruktorilla
+    henkilo1.setNimi("Matti Meikäläinen");
+    henkilo1.setIka(25);
+    henkilo1.tulostaTiedot();
+    
+    cout << endl << "=== Tehtävä 3: Käytetään konstruktoria ===" << endl;
+    Henkilo henkilo2("Maija Virtanen", 30);
+    henkilo2.tulostaTiedot();
+    
+    cout << endl << "=== Käytetään gettereita ===" << endl;
+    cout << "Henkilön nimi: " << henkilo2.getNimi() << endl;
+    cout << "Henkilön ikä: " << henkilo2.getIka() << endl;
+
+    return 0;
 }
--- h2/h2.h
+++ h2/h2.h
@@ -1,8 +1,37 @@
-﻿// h2.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
+﻿#pragma once
 
 #include <iostream>
+#include <string>
 
-// TODO: Reference additional headers your program requires here.
+// Tehtävä 1: Henkilo-luokka julkisilla jäsenmuuttujilla (HUONO TAPA - ei suositella)
+// Kommentoitu pois, koska käytetään parempaa toteutusta alla
+/*
+class Henkilo {
+public:
+    std::string nimi;
+    int ika;
+};
+*/
+
+// Tehtävä 2 & 3: Henkilo-luokka privaateilla jäsenmuuttujilla, 
+// Gettereilla, Settereilla, konstruktorilla ja tulostaTiedot-metodilla
+class Henkilo {
+private:
+    std::string nimi;
+    int ika;
+
+public:
+    // Tehtävä 3: Konstruktori
+    Henkilo(std::string nimi, int ika);
+
+    // Tehtävä 2: Getterit
+    std::string getNimi() const;
+    int getIka() const;
+
+    // Tehtävä 2: Setterit
+    void setNimi(std::string uusiNimi);
+    void setIka(int uusiIka);
+
+    // Tehtävä 2: Tulostusmetodi
+    void tulostaTiedot() const;
+};
