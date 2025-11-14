3c1a491a40f1c59f1524f07220aa3dbd6163a090
Tommi Lipponen <tlippone24@students.oamk.fi>
2025-11-14 17:50:21 +0200

    Corollat lisätty

---
+++ H2b/Car.cpp
@@ -0,0 +1,36 @@
+﻿#include "Car.h"
+#include <vector>
+
+using namespace std;
+
+// Konstruktorin toteutus
+Car::Car(string b, string m, int y) : brand(b), model(m), yearModel(y) {
+}
+
+// printData-metodin toteutus
+void Car::printData() const {
+    cout << "Brand: " << brand << ", Model: " << model << ", Year: " << yearModel << endl;
+}
+
+int main()
+{
+    // Luodaan vektori autoille
+    vector<Car> carList;
+    
+    // Lisätään kolme Car-oliota listaan
+    carList.push_back(Car("Toyota", "Corolla", 2020));
+    carList.push_back(Car("Volkswagen", "Golf", 2019));
+    carList.push_back(Car("Ford", "Focus", 2021));
+    
+    // Tulostetaan toisen alkion tiedot (indeksi 1)
+    cout << "Second car in the list:" << endl;
+    carList[1].printData();
+    
+    cout << "\nAll cars in the list:" << endl;
+    // Tulostetaan kaikki autot käyttämällä for-silmukkaa
+    for (const auto& car : carList) {
+        car.printData();
+    }
+    
+    return 0;
+}
---
+++ H2b/Car.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+
+// TODO: Viittaa lisäksi muihin otsikkotiedostoihin, joita ohjelmasi tarvitsee.
+
+class Car {
+private:
+    std::string brand;
+    std::string model;
+    int yearModel;
+
+public:
+    // Konstruktori
+    Car(std::string b, std::string m, int y);
+    
+    // Metodi auton tietojen tulostamiseen
+    void printData() const;
+};
